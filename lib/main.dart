21748328fb325b868f0cf9a778014f3eac0efe49
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-04-01 17:33:38 +0300

    add navigation bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,220 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'Returning Data',
-      home: HomeScreen(),
-    ),
-  );
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    title: 'Navigation Basics',
+    home: HomeScreen(),
+  ));
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    Widget progressSection = Container(
+
+    );
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: <Widget>[
+            IconButton(
+              color: Colors.amber,
+              icon: const Icon(Icons.home),
+              tooltip: 'Go to home screen',
+              onPressed: () {},
+            ),
+            Text(
+              'HOME',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: Colors.amber,
+              ),
+            )
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            IconButton(
+              color: Colors.amber,
+              icon: const Icon(Icons.screen_lock_portrait),
+              tooltip: 'Go to home screen',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SecondScreen()),
+                );
+              },
+            ),
+            Text(
+              'SECOND SCREEN',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: Colors.amber,
+              ),
+            )
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            IconButton(
+              color: Colors.amber,
+              icon: const Icon(Icons.wb_sunny_outlined),
+              tooltip: 'Go to home screen',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SelectThemeScreen()),
+                );
+              },
+            ),
+            Text(
+              'THEME',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: Colors.amber,
+              ),
+            )
+          ],
+        )
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+      ),
+      body: Container(
+        color: Colors.brown,
+        child: Column(
+          children: [
+            Flexible(
+              flex: 3,
+              child: Container(
+                color: Colors.purple,
+                child: buttonSection,
+              ),
+            ),
+
+            Container(
+              height: 70,
+              color: Colors.black45,
+              child: buttonSection,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Returning Data Demo'),
+        title: const Text('Second Screen'),
       ),
-      body: const Center(
-        child: SelectThemeButton(),
+      body: Center(
+        child: Text(
+          'Will be implemented later',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Colors.amber,
+          ),
+        ),
       ),
     );
   }
 }
 
-class SelectThemeButton extends StatelessWidget {
-  const SelectThemeButton({Key? key}) : super(key: key);
+class SelectThemeScreen extends StatefulWidget {
+  const SelectThemeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _navigateAndDisplayTheme(context);
-      },
-      child: const Text('Pick an option, any option!'),
-    );
-  }
-
-  // A method that launches the SelectionScreen and awaits the result from
-  // Navigator.pop.
-  void _navigateAndDisplayTheme(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SelectThemeScreen()),
-    );
-
-    // After the Selection Screen returns a result, hide any previous snackbars
-    // and show the new result.
-    ScaffoldMessenger.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text('$result')));
-  }
+  State<SelectThemeScreen> createState() => _SelectThemeScreenState();
 }
 
-class SelectThemeScreen extends StatelessWidget {
-  const SelectThemeScreen({Key? key}) : super(key: key);
+class _SelectThemeScreenState extends State<SelectThemeScreen> {
+  bool _selectLightTheme = true;
+  bool _selectDarkTheme = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pick an option'),
-        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 15.0,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(100, 100),
-                  shape: const CircleBorder(),
+        child: Container(
+          color: Colors.yellow,
+          margin: const EdgeInsets.all(20.0),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Switch(
+                  activeTrackColor: Colors.lightGreenAccent,
+                  activeColor: Colors.green,
+                  value: _selectLightTheme,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _selectLightTheme = value;
+                    });
+                    Navigator.pop(context, _selectLightTheme);
+                  },
                 ),
-                onPressed: () {
-                  // Close the screen and return "Yep!" as the result.
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: const Text('Yep!'),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(100, 100),
-                  shape: const CircleBorder(),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectLightTheme = false;
+                    });
+                    Navigator.pop(context, _selectLightTheme);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(100, 100),
+                    shape: const CircleBorder(),
+                    primary:
+                        _selectLightTheme ? Colors.red[500] : Colors.red[300],
+                    onPrimary: Colors.white,
+                  ),
+                  child: const Text(
+                    'Dark mode',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                onPressed: () {
-                  // Close the screen and return "Nope." as the result.
-                  Navigator.pop(context, 'Nope.');
-                },
-                child: const Text('Nope.'),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
