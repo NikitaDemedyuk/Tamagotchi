5f8a5eaaa1119367451a38ca005178f960469185
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-04-02 08:14:55 +0300

    add dark theme to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:tamagotchi/theme/theme_constants.dart';
+import 'package:tamagotchi/theme/theme_manager.dart';
+
+ThemeManager _themeManager = ThemeManager();
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData.light(),
-    title: 'Navigation Basics',
-    home: HomeScreen(),
-  ));
+  runApp(HomeScreen());
 }
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -20,9 +21,30 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
+
+  @override
+  void dispose() {
+    _themeManager.removeListener(themeListener);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _themeManager.addListener(themeListener);
+    super.initState();
+  }
+
+  themeListener() {
+    if (mounted) {
+      setState(() {
+
+      });
+    }
+  }
+
   List<Widget> _widgetOptions = <Widget> [
     Container(
-      color: Colors.brown,
+
     ),
     Container(
       color: Colors.red,
@@ -40,13 +62,6 @@ class _HomeScreenState extends State<HomeScreen> {
     ),
     Container(
       color: Colors.black12,
-      child: Switch(
-        activeTrackColor: Colors.lightGreenAccent,
-        activeColor: Colors.green,
-        value: false,
-        onChanged: (bool value) {
-        },
-      ),
     ),
   ];
 
@@ -58,130 +73,41 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home screen'),
-      ),
-      body: Container(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem> [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'HOME',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.screen_lock_portrait),
-            label: 'SECOND SCREEN',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.wb_sunny_outlined),
-            label: 'THEME',
-          ),
-        ],
-        selectedItemColor: Colors.amber,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
-
-class SecondScreen extends StatefulWidget {
-  const SecondScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SecondScreen> createState() => _SecondScreenState();
-}
-
-class _SecondScreenState extends State<SecondScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Screen'),
-      ),
-      body: Center(
-        child: Text(
-          'Will be implemented later',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
-            color: Colors.amber,
-          ),
+    return MaterialApp(
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: _themeManager.themeMode,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home screen'),
+          actions: [Switch(value: _themeManager.themeMode == ThemeMode.dark, onChanged: (newValue) {
+            _themeManager.toogleTheme(newValue);
+          })],
+        ),
+        body: Container(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem> [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'HOME',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.screen_lock_portrait),
+              label: 'SECOND SCREEN',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.wb_sunny_outlined),
+              label: 'THEME',
+            ),
+          ],
+          selectedItemColor: Colors.amber,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
       ),
     );
   }
 }
 
-class SelectThemeScreen extends StatefulWidget {
-  const SelectThemeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SelectThemeScreen> createState() => _SelectThemeScreenState();
-}
-
-class _SelectThemeScreenState extends State<SelectThemeScreen> {
-  bool _selectLightTheme = true;
-  bool _selectDarkTheme = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick an option'),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.yellow,
-          margin: const EdgeInsets.all(20.0),
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Switch(
-                  activeTrackColor: Colors.lightGreenAccent,
-                  activeColor: Colors.green,
-                  value: _selectLightTheme,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _selectLightTheme = value;
-                    });
-                    Navigator.pop(context, _selectLightTheme);
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _selectLightTheme = false;
-                    });
-                    Navigator.pop(context, _selectLightTheme);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(100, 100),
-                    shape: const CircleBorder(),
-                    primary:
-                        _selectLightTheme ? Colors.red[500] : Colors.red[300],
-                    onPrimary: Colors.white,
-                  ),
-                  child: const Text(
-                    'Dark mode',
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
