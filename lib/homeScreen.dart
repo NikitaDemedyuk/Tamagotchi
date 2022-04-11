45fc8199e6487c00d44da4aef60edc84864206cc
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-04-11 12:05:48 +0300

    add animated buttons

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -15,7 +15,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final List _isFeed = [false, false, false, false, false];
   final List _isHappy = [false, false, false, false, false];
-  int index = 0;
+
+  int indexFeed = 0;
+  int indexHappy = 0;
 
   @override
   void initState() {
@@ -27,134 +29,124 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
-
-  void _toggleFeed(int index) {
+  void _toggleFeed(int indexFeed) {
     setState(() {
-      if (!_isFeed[index]) {
-        _isFeed[index] = true;
+      if (!_isFeed[indexFeed]) {
+        _isFeed[indexFeed] = true;
       }
     });
   }
 
-  void _togglePlay(int index) {
+  void _toggleHappy(int indexHappy) {
     setState(() {
-      if (_isFeed[index]) {
-        _isFeed[index] = false;
+      if (!_isHappy[indexHappy]) {
+        _isHappy[indexHappy] = true;
+      }
+    });
+  }
+
+  void _togglePlay(int indexPlay) {
+    setState(() {
+      if (_isFeed[indexPlay]) {
+        _isFeed[indexPlay] = false;
+      }
+    });
+  }
+
+  void _toggleSad(int indexHappy) {
+    setState(() {
+      if (_isHappy[indexHappy]) {
+        _isHappy[indexHappy] = false;
       }
     });
   }
 
   Widget iconsSectionFeed() {
-    return Column(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            _isFeed[0] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-            _isFeed[1] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-            _isFeed[2] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-            _isFeed[3] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-            _isFeed[4] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-          ],
-        ),
+        _isFeed[0] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
+        _isFeed[1] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
+        _isFeed[2] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
+        _isFeed[3] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
+        _isFeed[4] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
       ],
     );
   }
 
   Widget iconsSectionHappy() {
-    return Column(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            _isHappy[0] ? Icon(Icons.emoji_emotions) : Icon(Icons.emoji_emotions_outlined),
-            _isHappy[1] ? Icon(Icons.emoji_emotions) : Icon(Icons.emoji_emotions_outlined),
-            _isHappy[2] ? Icon(Icons.emoji_emotions) : Icon(Icons.emoji_emotions_outlined),
-            _isHappy[3] ? Icon(Icons.emoji_emotions) : Icon(Icons.emoji_emotions_outlined),
-            _isHappy[4] ? Icon(Icons.emoji_emotions) : Icon(Icons.emoji_emotions_outlined),
-          ],
-        ),
+        _isHappy[0]
+            ? Icon(Icons.emoji_emotions)
+            : Icon(Icons.emoji_emotions_outlined),
+        _isHappy[1]
+            ? Icon(Icons.emoji_emotions)
+            : Icon(Icons.emoji_emotions_outlined),
+        _isHappy[2]
+            ? Icon(Icons.emoji_emotions)
+            : Icon(Icons.emoji_emotions_outlined),
+        _isHappy[3]
+            ? Icon(Icons.emoji_emotions)
+            : Icon(Icons.emoji_emotions_outlined),
+        _isHappy[4]
+            ? Icon(Icons.emoji_emotions)
+            : Icon(Icons.emoji_emotions_outlined),
       ],
     );
   }
 
+  Widget buttonActionOnImage(Color color, double endRadius, double opacity,
+      int secondDurtion, int action) {
+    return AvatarGlow(
+      glowColor: color,
+      duration: Duration(seconds: secondDurtion),
+      child: ElevatedButton(
+        onPressed: () {
+          if (action == 1) {
+            if (indexHappy < 5) {
+              _toggleHappy(indexHappy);
+              ++indexHappy;
+            }
+          }
+          if (action == 2) {
+            if (indexFeed == 4) {
+              _toggleFeed(indexFeed);
+              ++indexFeed;
+            }
+            if (indexFeed < 4) {
+              _toggleFeed(indexFeed);
+              _toggleFeed(indexFeed + 1);
+              indexFeed += 2;
+            }
+          }
+
+          if (action == 3) {
+            if (indexHappy == 0) {
+              _toggleSad(indexHappy);
+            } else if (indexHappy > 0) {
+              --indexHappy;
+              _toggleSad(indexHappy);
+            }
+          }
+        },
+        child: null,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.amber.withOpacity(opacity),
+          shape: CircleBorder(),
+        ),
+      ),
+      endRadius: endRadius,
+    );
+  }
+
   Widget imageSection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Container(
-            //color: Colors.tealAccent,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  iconsSectionHappy(),
-                  iconsSectionFeed(),
-                ],
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              Image.asset(
-                'images/ben2011.webp',
-                fit: BoxFit.fill,
-              ),
-              Align(
-                alignment: Alignment.topCenter,
-                child: AvatarGlow(
-                  glowColor: Colors.red,
-                  duration: Duration(seconds: 2),
-                  child: ElevatedButton(
-                    onPressed: (){
-                      if (index == 4) {
-                        _toggleFeed(index);
-                        ++index;
-                      }
-                      if (index < 4) {
-                        _toggleFeed(index);
-                        _toggleFeed(index + 1);
-                        index += 2;
-                      }
-                    },
-                    child: null,
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.amber.withOpacity(0.01),
-                      shape: CircleBorder(),
-                    ),
-                  ),
-                  endRadius: 100,
-                ),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: AvatarGlow(
-                  glowColor: Colors.amber,
-                  duration: Duration(seconds: 2),
-                  child: ElevatedButton(
-                      onPressed: (){
-                      },
-                      child: null,
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.amber.withOpacity(0.01),
-                        shape: CircleBorder(),
-                      ),
-                  ),
-                  endRadius: 100,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
+    return Image.asset(
+      'images/ben2011.webp',
+      fit: BoxFit.fill,
     );
   }
 
@@ -174,38 +166,20 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       onPressed: () {
         if (indexButton == 1) {
-          if (index == 0) {
-            _togglePlay(index);
-          } else if (index > 0) {
-            --index;
-            _togglePlay(index);
+          if (indexFeed == 0) {
+            _togglePlay(indexFeed);
+          } else if (indexFeed > 0) {
+            --indexFeed;
+            _togglePlay(indexFeed);
           }
-        } else if (indexButton == 2) {
-          if (index < 5) {
-            _toggleFeed(index);
-            ++index;
+        }
+        if (indexButton == 2) {
+          if (indexFeed < 5) {
+            _toggleFeed(indexFeed);
+            ++indexFeed;
           }
         }
       },
-    );
-  }
-
-  Widget buttonSection() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        buttonAction(Icons.videogame_asset, 70, 70, 35.0, 1),
-        buttonAction(Icons.set_meal_rounded, 90, 90, 40.0, 2),
-        buttonAction(Icons.videocam, 70, 70, 35.0, 3),
-      ],
-    );
-  }
-
-  Widget buttonsColumn() {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      //color: Colors.amber,
-      child: buttonSection(),
     );
   }
 
@@ -220,11 +194,78 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Expanded(
               flex: 1,
-              child: buttonsColumn(),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                //color: Colors.amber,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buttonAction(Icons.videogame_asset, 70, 70, 35.0, 1),
+                    buttonAction(Icons.set_meal_rounded, 90, 90, 40.0, 2),
+                    buttonAction(Icons.videocam, 70, 70, 35.0, 3),
+                  ],
+                ),
+              ),
             ),
             Expanded(
               flex: 3,
-              child: imageSection(),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          iconsSectionHappy(),
+                          iconsSectionFeed(),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      //color: Colors.purple,
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: <Widget>[
+                          imageSection(),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child:
+                                buttonActionOnImage(Colors.red, 90, 0.01, 2, 3),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: buttonActionOnImage(Colors.red, 120, 0.01, 2, 3),
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: buttonActionOnImage(Colors.green, 60, 0.01, 2, 1),
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: buttonActionOnImage(Colors.amber, 100, 0.01, 2, 2),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: buttonActionOnImage(Colors.green, 70, 0.01, 2, 1),
+                          ),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: buttonActionOnImage(Colors.green, 70, 0.01, 2, 1),
+                          ),
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: buttonActionOnImage(Colors.red, 110, 0.01, 2, 3),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
