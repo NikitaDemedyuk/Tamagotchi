097148f94d94b1317868c02b78f1a84684fa2680
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-04-11 12:41:35 +0300

    add text labels to progress bars and remove unnecessary button

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,6 +1,5 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:tamagotchi/main.dart';
 import 'package:tamagotchi/theme/theme_constants.dart';
@@ -66,11 +65,18 @@ class _HomeScreenState extends State<HomeScreen> {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        _isFeed[0] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-        _isFeed[1] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-        _isFeed[2] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-        _isFeed[3] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
-        _isFeed[4] ? Icon(Icons.fastfood) : Icon(Icons.fastfood_outlined),
+        const Text(
+          'Feed bar',
+          style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'Arial'
+          ),
+        ),
+        _isFeed[0] ? const Icon(Icons.fastfood) : const Icon(Icons.fastfood_outlined),
+        _isFeed[1] ? const Icon(Icons.fastfood) : const Icon(Icons.fastfood_outlined),
+        _isFeed[2] ? const Icon(Icons.fastfood) : const Icon(Icons.fastfood_outlined),
+        _isFeed[3] ? const Icon(Icons.fastfood) : const Icon(Icons.fastfood_outlined),
+        _isFeed[4] ? const Icon(Icons.fastfood) : const Icon(Icons.fastfood_outlined),
       ],
     );
   }
@@ -80,21 +86,28 @@ class _HomeScreenState extends State<HomeScreen> {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
+        const Text(
+          'Happy bar',
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: 'Arial'
+          ),
+        ),
         _isHappy[0]
-            ? Icon(Icons.emoji_emotions)
-            : Icon(Icons.emoji_emotions_outlined),
+            ? const Icon(Icons.emoji_emotions)
+            : const Icon(Icons.emoji_emotions_outlined),
         _isHappy[1]
-            ? Icon(Icons.emoji_emotions)
-            : Icon(Icons.emoji_emotions_outlined),
+            ? const Icon(Icons.emoji_emotions)
+            : const Icon(Icons.emoji_emotions_outlined),
         _isHappy[2]
-            ? Icon(Icons.emoji_emotions)
-            : Icon(Icons.emoji_emotions_outlined),
+            ? const Icon(Icons.emoji_emotions)
+            : const Icon(Icons.emoji_emotions_outlined),
         _isHappy[3]
-            ? Icon(Icons.emoji_emotions)
-            : Icon(Icons.emoji_emotions_outlined),
+            ? const Icon(Icons.emoji_emotions)
+            : const Icon(Icons.emoji_emotions_outlined),
         _isHappy[4]
-            ? Icon(Icons.emoji_emotions)
-            : Icon(Icons.emoji_emotions_outlined),
+            ? const Icon(Icons.emoji_emotions)
+            : const Icon(Icons.emoji_emotions_outlined),
       ],
     );
   }
@@ -136,7 +149,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: null,
         style: ElevatedButton.styleFrom(
           primary: Colors.amber.withOpacity(opacity),
-          shape: CircleBorder(),
+          shape: const CircleBorder(),
         ),
       ),
       endRadius: endRadius,
@@ -155,7 +168,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         fixedSize: Size(width, height),
-        shape: CircleBorder(),
+        shape: const CircleBorder(),
       ),
       child: Align(
         alignment: Alignment.center,
@@ -187,38 +200,35 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TAMAGOTCHI'),
+        title: const Text('TAMAGOTCHI'),
       ),
       body: Container(
-        child: Row(
+        child: Column(
           children: [
             Expanded(
               flex: 1,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                //color: Colors.amber,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    buttonAction(Icons.videogame_asset, 70, 70, 35.0, 1),
-                    buttonAction(Icons.set_meal_rounded, 90, 90, 40.0, 2),
-                    buttonAction(Icons.videocam, 70, 70, 35.0, 3),
-                  ],
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  iconsSectionHappy(),
+                  iconsSectionFeed(),
+                ],
               ),
             ),
             Expanded(
               flex: 3,
-              child: Column(
+              child: Row(
                 children: [
                   Expanded(
                     flex: 1,
                     child: Container(
+                      padding: const EdgeInsets.all(10),
+                      //color: Colors.amber,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          iconsSectionHappy(),
-                          iconsSectionFeed(),
+                          buttonAction(Icons.videogame_asset, 70, 70, 35.0, 1),
+                          buttonAction(Icons.set_meal_rounded, 90, 90, 40.0, 2),
                         ],
                       ),
                     ),
@@ -234,31 +244,37 @@ class _HomeScreenState extends State<HomeScreen> {
                           Align(
                             alignment: Alignment.bottomRight,
                             child:
-                                buttonActionOnImage(Colors.red, 90, 0.01, 2, 3),
+                            buttonActionOnImage(Colors.red, 90, 0.01, 2, 3),
                           ),
                           Align(
                             alignment: Alignment.bottomLeft,
-                            child: buttonActionOnImage(Colors.red, 120, 0.01, 2, 3),
+                            child: buttonActionOnImage(
+                                Colors.red, 120, 0.01, 2, 3),
                           ),
                           Align(
                             alignment: Alignment.centerRight,
-                            child: buttonActionOnImage(Colors.green, 60, 0.01, 2, 1),
+                            child: buttonActionOnImage(
+                                Colors.green, 60, 0.01, 2, 1),
                           ),
                           Align(
                             alignment: Alignment.center,
-                            child: buttonActionOnImage(Colors.amber, 100, 0.01, 2, 2),
+                            child: buttonActionOnImage(
+                                Colors.amber, 100, 0.01, 2, 2),
                           ),
                           Align(
                             alignment: Alignment.topRight,
-                            child: buttonActionOnImage(Colors.green, 70, 0.01, 2, 1),
+                            child: buttonActionOnImage(
+                                Colors.green, 70, 0.01, 2, 1),
                           ),
                           Align(
                             alignment: Alignment.topLeft,
-                            child: buttonActionOnImage(Colors.green, 70, 0.01, 2, 1),
+                            child: buttonActionOnImage(
+                                Colors.green, 70, 0.01, 2, 1),
                           ),
                           Align(
                             alignment: Alignment.topCenter,
-                            child: buttonActionOnImage(Colors.red, 110, 0.01, 2, 3),
+                            child: buttonActionOnImage(
+                                Colors.red, 110, 0.01, 2, 3),
                           ),
                         ],
                       ),
